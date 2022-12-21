4666cf8fbcdc22a982dd69d3c21f28af95d9efc0
Benoit <cantebenoit@hotmail.com>
2022-12-21 18:26:04 +0100

    files
    initial commit

---
+++ sort-list.js
@@ -0,0 +1,240 @@
+let items = "";
+let goodAnswers = "";
+let expectedArray = []
+// random de manière schlag du tableau
+
+let firstTime = true;
+
+window.onload = function init(){
+	let input = prompt("Ecrivez la phrase à remettre dans l'ordre en séparant les 'blocs' par des virgules");
+	//chat = "1,2,3";
+	expectedArray = input.split(",");
+	let originArray = expectedArray.slice().sort((a, b) => 0.5 - Math.random()); 
+	//let originArray = expectedArray
+    var ul = document.getElementById("sortlist");
+	
+	for (let it=0; it<originArray.length; it++) {
+		var li = document.createElement("li");
+		li.appendChild(document.createTextNode(originArray[it]));
+		ul.appendChild(li);
+	}
+	slist(document.getElementById("sortlist"));
+	
+	let button = document.querySelector('#myButton');
+
+	button.addEventListener('click', ()=>{
+	  checkAnswers();
+	})
+	confettis();
+  };
+  
+function slist (target) {
+	
+  // (A) SET CSS + GET ALL LIST ITEMS
+  target.classList.add("slist");
+  items = target.getElementsByTagName("li"), current = null;
+
+  // (B) MAKE ITEMS DRAGGABLE + SORTABLE
+  for (let i of items) {
+    // (B1) ATTACH DRAGGABLE
+    i.draggable = true;
+
+    // (B2) DRAG START - YELLOW HIGHLIGHT DROPZONES
+    i.ondragstart = (ev) => {
+      current = i;
+      for (let it of items) {
+        if (it != current) { it.classList.add("hint"); }
+      }
+    };
+
+    // (B3) DRAG ENTER - RED HIGHLIGHT DROPZONE
+    i.ondragenter = (ev) => {
+      if (i != current) { i.classList.add("active"); }
+    };
+
+    // (B4) DRAG LEAVE - REMOVE RED HIGHLIGHT
+    i.ondragleave = () => {
+      i.classList.remove("active");
+    };
+
+    // (B5) DRAG END - REMOVE ALL HIGHLIGHTS
+    i.ondragend = () => { for (let it of items) {
+      it.classList.remove("hint");
+      it.classList.remove("active");
+    }};
+
+    // (B6) DRAG OVER - PREVENT THE DEFAULT "DROP", SO WE CAN DO OUR OWN
+    i.ondragover = (evt) => { evt.preventDefault(); };
+
+    // (B7) ON DROP - DO SOMETHING
+    i.ondrop = (evt) => {
+      evt.preventDefault();
+      if (i != current) {
+        let currentpos = 0, droppedpos = 0;
+        for (let it=0; it<items.length; it++) {
+          if (current == items[it]) { currentpos = it; }
+          if (i == items[it]) { droppedpos = it; }
+        }
+        if (currentpos < droppedpos) {
+          i.parentNode.insertBefore(current, i.nextSibling);
+        } else {
+          i.parentNode.insertBefore(current, i);
+        }
+    };
+  }
+}
+}
+
+function checkAnswers() {
+	
+	
+	let isGood = true;
+	for (let it=0; it<expectedArray.length; it++) {
+		items[it].classList.remove("good");
+		items[it].classList.remove("wrong");
+		
+		let a = expectedArray[it];
+		let b = items[it].textContent;
+		if (a != b && isGood) {
+			isGood = false;
+			console.log("Expected: "+a	+" got: "+b);
+		}
+	}
+	items = document.getElementById("sortlist").getElementsByTagName("li");
+	if (isGood) {
+		for (let it=0; it<items.length; it++) {
+			items[it].classList.add("good");
+			initConfetti();
+		}
+	}
+	else {
+		for (let it=0; it<items.length; it++) {
+			items[it].classList.add("wrong");
+		}
+	}
+}
+
+function show() {
+	document.getElementByClass('image').style.display = "block";
+	document.getElementByClass('btnID').style.display = "none";
+}
+
+function confettis(){
+	//-----------Var Inits--------------
+	canvas = document.getElementById("canvas");
+	ctx = canvas.getContext("2d");
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+	cx = ctx.canvas.width / 2;
+	cy = ctx.canvas.height / 2;
+
+	let confetti = [];
+	const confettiCount = 300;
+	const gravity = 0.5;
+	const terminalVelocity = 5;
+	const drag = 0.075;
+	const colors = [
+	{ front: 'red', back: 'darkred' },
+	{ front: 'green', back: 'darkgreen' },
+	{ front: 'blue', back: 'darkblue' },
+	{ front: 'yellow', back: 'darkyellow' },
+	{ front: 'orange', back: 'darkorange' },
+	{ front: 'pink', back: 'darkpink' },
+	{ front: 'purple', back: 'darkpurple' },
+	{ front: 'turquoise', back: 'darkturquoise' }];
+
+
+	//-----------Functions--------------
+	resizeCanvas = () => {
+	  canvas.width = window.innerWidth;
+	  canvas.height = window.innerHeight;
+	  cx = ctx.canvas.width;
+	  cy = ctx.canvas.height / 2;
+	};
+
+	randomRange = (min, max) => Math.random() * (max - min) + min;
+
+	initConfetti = () => {
+	  for (let i = 0; i < confettiCount; i++) {
+		confetti.push({
+		  color: colors[Math.floor(randomRange(0, colors.length))],
+		  dimensions: {
+			x: randomRange(10, 20),
+			y: randomRange(10, 30) },
+
+		  position: {
+			x: randomRange(0, canvas.width),
+			y: canvas.height - 1 },
+
+		  rotation: randomRange(0, 2 * Math.PI),
+		  scale: {
+			x: 1,
+			y: 1 },
+
+		  velocity: {
+			x: randomRange(-25, 25),
+			y: randomRange(0, -50) } });
+
+
+	  }
+	};
+
+	//---------Render-----------
+	render = () => {
+	  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+	  confetti.forEach((confetto, index) => {
+		let width = confetto.dimensions.x * confetto.scale.x;
+		let height = confetto.dimensions.y * confetto.scale.y;
+
+		// Move canvas to position and rotate
+		ctx.translate(confetto.position.x, confetto.position.y);
+		ctx.rotate(confetto.rotation);
+
+		// Apply forces to velocity
+		confetto.velocity.x -= confetto.velocity.x * drag;
+		confetto.velocity.y = Math.min(confetto.velocity.y + gravity, terminalVelocity);
+		confetto.velocity.x += Math.random() > 0.5 ? Math.random() : -Math.random();
+
+		// Set position
+		confetto.position.x += confetto.velocity.x;
+		confetto.position.y += confetto.velocity.y;
+
+		// Delete confetti when out of frame
+		if (confetto.position.y >= canvas.height) confetti.splice(index, 1);
+
+		// Loop confetto x position
+		if (confetto.position.x > canvas.width) confetto.position.x = 0;
+		if (confetto.position.x < 0) confetto.position.x = canvas.width;
+
+		// Spin confetto by scaling y
+		confetto.scale.y = Math.cos(confetto.position.y * 0.1);
+		ctx.fillStyle = confetto.scale.y > 0 ? confetto.color.front : confetto.color.back;
+
+		// Draw confetti
+		ctx.fillRect(-width / 2, -height / 2, width, height);
+
+		// Reset transform matrix
+		ctx.setTransform(1, 0, 0, 1, 0, 0);
+	  });
+
+	  // Fire off another round of confetti
+	  //if (confetti.length <= 10) initConfetti();
+
+	  window.requestAnimationFrame(render);
+	};
+
+	//---------Execution--------
+	//initConfetti();
+	render();
+
+	//----------Resize----------
+	window.addEventListener('resize', function () {
+	  resizeCanvas();
+	});
+
+	//------------Click------------
+	window.addEventListener('click', function () {
+	  //initConfetti();
+	});
+}
